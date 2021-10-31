1d17a4a5783864f45216607cbf9b8e365e6f38e5
matias97-coder <matiasortigoza8@gmail.com>
2021-10-31 01:47:26 -0300

    entraron los dinos

--- src/juego/Dinosaurio.java
+++ src/juego/Dinosaurio.java
@@ -13,7 +13,7 @@ public class Dinosaurio {
 	private String direccion;
 	private Laser poder;
 	private int ancho, alto;
-	private int cont = 6;
+
 	
 	
 	public Dinosaurio(int x, int y) {
@@ -22,7 +22,7 @@ public class Dinosaurio {
 		this.ancho=30;
 		this.alto=60;
 		this.poder = null;
-		this.imagen = Herramientas.cargarImagen("images/barbariana.png");
+		this.imagen = Herramientas.cargarImagen("images/dino_izq.png");
 	}
 
 	public int getX() {
@@ -43,33 +43,34 @@ public class Dinosaurio {
 
 	void moverIzquierda() {
 		this.x = this.x - 2;
+		this.imagen= Herramientas.cargarImagen("images/dino_izq.png");
 		this.direccion = "izquierda";
 		
 	}
 
 	void moverDerecha() {
 		this.x = this.x + 2;
+		this.imagen= Herramientas.cargarImagen("images/dino_der.png");
 		this.direccion = "derecha";
 		
 	}
 	
 	void caminar() {
 		if (this.direccion == "izquierda") {
+			;
 			moverIzquierda();
-		}if (this.direccion == "derecha")
+			}if (this.direccion == "derecha")
+			
+		
 			moverDerecha();
 		}
 
-
-	public int cantDinos() { //CHEQUEA LA CANTIDAD DE DINOS IN GAME
-		return cont;
-	}
 	
 	public boolean enElSuelo() { // Chequea si barbarianna esta tocando alguno de los niveles (VER EJE DE Y) En el eje x sólo toma las partes amarillas.
-		if ((this.y == 90 - this.getAlto()/2 && this.getX() + this.getAncho()/2>200)|| 
-				(this.y == 210 - this.getAlto()/2 && this.getX() -this.getAncho()/2<600) || 
-				(this.y == 330 - this.getAlto()/2 && this.getX() +this.getAncho()/2>200)||
-				(this.y == 450 - this.getAlto()/2 && this.getX() -this.getAncho()/2<600)|| this.y == 570)
+		if ((this.y == 120 - this.getAlto()/2 && this.getX() + this.getAncho()/2>200)|| 
+				(this.y == 240 - this.getAlto()/2 && this.getX() -this.getAncho()/2<600) || 
+				(this.y == 360 - this.getAlto()/2 && this.getX() +this.getAncho()/2>200)||
+				(this.y == 480 - this.getAlto()/2 && this.getX() -this.getAncho()/2<600)|| this.y == 570)
 			return true;
 		return false;
 	}
@@ -88,9 +89,9 @@ public class Dinosaurio {
 	}
 	
 	public void dibujarse(Entorno entorno) {
-		//entorno.dibujarImagen(imagen, this.x, this.y, 0, 0.1); // 0.1= tamaño de la imagen
-		entorno.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.GREEN);
-		direccion = "izquierda";
+		entorno.dibujarImagen(imagen, this.x, this.y, 0, 0.2); // 0.1= tamaño de la imagen
+		//entorno.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.GREEN);
+		
 	}
 	
 	public void crearLaser(Entorno entorno) {
@@ -99,77 +100,77 @@ public class Dinosaurio {
 			this.poder = new Laser(this.x, this.y, direccion);
 		}
 	
+	
+
 	public void efectuarLaser(Entorno entorno) {
-			
-			this.poder.dibujarse(entorno);
-			
-			switch (this.poder.getDireccion()) {
-			
-			case "derecha":
-				this.poder.moverDerecha();
-				break;
-				
-			case "izquierda":
-				this.poder.moverIzquierda();
-				break;
-			}
-			
-			// Cuando el rayo toque algun borde vuelvo el objeto rayo a null 
 		
-			if (this.poder.getX() <= 0 || this.poder.getX() >= entorno.ancho()  ) {	
-				this.poder = null;
-			}
-		}
-	
-		//1
-		 public boolean choqueRayoDino(Rayo rayo) {//colision entre reyo de barbariana y los dinos
-		    	
-			if (rayo == null) 
-		    		return false;
-		    	
-		    if(Math.abs(this.x-rayo.getX())<=15 && Math.abs(this.y-rayo.getY())<=27){
-			        return true;
-			    }
-		    
-		    return false;
-		    }
-	
-		 
-		//2
-		 public boolean choqueBarbariannaDino(Barbarianna barb) {//colision entre los personajes 
-			if (barb != null) 
-		    	if(Math.abs(this.x-barb.getX())<=25 && Math.abs(this.y-barb.getY())<=27){
-			        return true;
-			    }
-		    return false;
-		    
-		    }
-		 
-		//3
-		public boolean choqueLaserBarba(Barbarianna barb) {//colision entre laser de los dinos y barbarianna 
-			 if (this.poder == null) 
-		    		return false;
-		    	
-		     if(Math.abs(this.poder.getX() -barb.getX())<=15 && Math.abs(this.poder.getY() -barb.getY())<=27){
-			        return true;
-			    }
-		     return false;
-		    }
-		 
-		 //4
-		 public boolean choqueRayoLaser(Rayo rayo) {//colision entre rayo y laser
-			 
-			 if (rayo == null) 
-				 
-		    		return false;
-		    	
-		     if(Math.abs(this.poder.getX()-rayo.getX())<=10 && Math.abs(this.poder.getY()-rayo.getY())<=27){
-			        return true;
-			    }
-		     
-		    return false;
+		this.poder.dibujarse(entorno);
+		
+		switch (this.poder.getDireccion()) {
+		
+		case "derecha":
+			this.poder.moverDerecha();
+			break;
 			
-		 }
+		case "izquierda":
+			this.poder.moverIzquierda();
+			break;
+		}
+		
+		// Cuando el rayo toque algun borde vuelvo el objeto rayo a null 
+	
+		if (this.poder.getX() <= 0 || this.poder.getX() >= entorno.ancho()  ) {	
+			this.poder = null;
+		}
+	}
+	
+	 //1
+	 public boolean choqueRayoDino(Rayo rayo) {//colision entre reyo de barbariana y los dinos
+	    	
+		 if (rayo == null) 
+	    		return false;
+	    	
+	    	if(Math.abs(this.x-rayo.getX())<=15 && Math.abs(this.y-rayo.getY())<=27){
+		        return true;
+		    }
+	    	
+	    	return false;
+	    }
+	 
+	 
+	 
+		//2
+	 public boolean choqueBarbariannaDino(Barbarianna barb) {//colision entre los personajes 
+		 if (barb != null) 
+	    	if(Math.abs(this.x-barb.getX())<=25 && Math.abs(this.y-barb.getY())<=27){
+		        return true;
+		    }
+	    return false;
+	    
+	    }
+	 
+
+	 /*
+	 //4
+	 public boolean choqueRayoLaser(Rayo rayo,Laser laser) {//colision entre rayo y laser
+		 if (rayo == null ) 
+			 
+	    		return false;
 		 
+		 if (laser==null)
+			 return false;
+	    	
+	    if(Math.abs(laser.getX()-rayo.getX())<=10 && Math.abs(laser.getY()-rayo.getY())<=27){
+		        return true;
+		    }
+	    
+	    return false;
+		
+	 }
+	 */
+	 
+	 
+	 
+	
 	
 }
